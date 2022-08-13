c8082f4250239c61324a05ac6ca58362860ce8d9
Travis Storie <storie25@outlook.com>
2022-08-13 07:38:17 -0700

    Added keys to places.js file

--- models/places.js
+++ models/places.js
@@ -1,10 +1,12 @@
 module.exports = [{
+    key: 0,
     name: 'H-Thai-ML',
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
     pic: '/images/thai-food.jpg'
 }, {
+    key: 1,
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
