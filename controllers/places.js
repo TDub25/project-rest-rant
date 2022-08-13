94d3cf33ceddf4d0bf04ed3de87c8e4a6406520c
Travis Storie <storie25@outlook.com>
2022-08-13 07:41:39 -0700

    Fixed controllers file

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,69 @@
 const router = require('express').Router()
+const e = require('express')
 const places = require('../models/places.js')
 
+router.get('/',(req,res)=>{
+  res.render('places/index',{places})
+}) 
+//View
+router.get('/new', (req,res)=>{
+  res.render('places/new')
+}) 
+router.get('/:id', (req,res)=>{
+  let id = Number(req.params.id)
+  if(isNaN(id)){
+    res.render('error404')
+  }
+  else if (!places[id]){
+    res.render('error404')
+  }
+  else {
+  res.render('places/show',{place: places[id],id} )
+  }
+})  
+//New
 router.post('/', (req, res) => {
-    if (!req.body.pic) {
-      // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-  })
-  
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  req.body.key = places.length+1
+  places.push(req.body)
+  res.redirect('/places')
+})
+//Edit
+router.get('/:id/edit', (req, res) => {
+let id = Number(req.params.id)
+if (isNaN(id)) {
+    res.render('error404')
+}
+else if (!places[id]) {
+    res.render('error404')
+}
+else {
+  res.render('places/edit', { place: places[id] })
+}
 })
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
-  })  
+//Delete
+router.delete('/:id', (req, res) => {
+let id = Number(req.params.id)
+if (isNaN(id)) {
+  res.render('error404')
+}
+else if (!places[id]) {
+  res.render('error404')
+}
+else {
+  places.splice(id,1)
+  res.redirect('/places')
+}
+})
 
 module.exports = router
