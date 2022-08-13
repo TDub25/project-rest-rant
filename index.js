9f8bd06eb04594476c23a32979cbbe595c1add9a
Travis Storie <storie25@outlook.com>
2022-08-13 10:54:24 -0700

    Minor change on index file

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use('/places',require('./controllers/places'))
+
 app.get('/',(req,res)=>{
     res.render('home')
 })
