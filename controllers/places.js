0e0ee1d5a6a47eefe8a2daf8efb1045b4f6080bb
Travis Storie <storie25@outlook.com>
2022-08-13 17:17:31 -0700

    Added activity be-7-rest-rant-part-8 changes

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const e = require('express')
 const places = require('../models/places.js')
+const db = require('../models')
 
-router.get('/',(req,res)=>{
+//Commented out lines 6 through 70 in case new code is inoperable
+/* router.get('/',(req,res)=>{
   res.render('places/index',{places})
 }) 
 //View
@@ -64,6 +66,57 @@ else {
   places.splice(id,1)
   res.redirect('/places')
 }
+})
+
+module.exports = router */
+router.get('/', (req, res) => {
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err) 
+      res.render('error404')
+    })
+})
+
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  res.send('GET /places/:id stub')
+})
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
 })
 
 module.exports = router
--- models/places.js
+++ models/places.js
@@ -1,15 +1,12 @@
-module.exports = [{
-    key: 0,
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thai-food.jpg'
-}, {
-    key: 1,
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/cafe-food.jpg'
-}]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
